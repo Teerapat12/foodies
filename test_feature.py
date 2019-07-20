4e608984bc54883d7b65232316bb24bd943c1a9f
Teerapat12 <teerapat.time12@gmail.com>
2019-07-21 00:00:37 +0700

    Proof of concept for ranking

--- test_feature.py
+++ test_feature.py
@@ -4,3 +4,4 @@ import utils as data_utils
 shop = data_utils.read_csv("shop")
 review = data_utils.read_csv("review")
 food_nationality = data_utils.read_csv("food_nationality")
+
--- utils.py
+++ utils.py
@@ -1,3 +1,22 @@
 import pandas as pd
 def read_csv(table_name):
     return pd.read_csv("data/foodies_table_%s.csv"%table_name)
+
+
+ [
+     'tel_count',
+     'has_website',
+     'has_line',
+     'has_facebook',
+     'has_instagram',
+     'has_all',
+     'has_carpark_i',
+     'open_period_count',
+     'shop_average_price',
+     'near_shop_counts',
+     'description_length',
+     'no_room',
+     'no_floor',
+     'shop_average_price',
+     'last_order'
+ ]
