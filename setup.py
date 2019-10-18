b5ed424df52dc6bd44be5fe352ebee06e504fea6
Teerapat12 <teerapat.time12@gmail.com>
2019-10-19 00:15:38 +0700

    add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='foodies',
+      version='0.1',
+      description='Repository for Foodies Recommender System',
+      url='https://github.com/Teerapat12/foodies',
+      author='Teerapat (Time)',
+      author_email='teerapat.time12@gmail.com',
+      license='MIT',
+      packages=[
+          'foodies',
+          'pandas',
+          'numpy'
+      ],
+      zip_safe=False)
